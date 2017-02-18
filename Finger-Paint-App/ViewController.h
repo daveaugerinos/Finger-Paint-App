24dab82d920f742ca3b060bb060d22162680a5cd
daveaugerinos <dave.augerinos@gmail.com>
2017-02-18 14:13:34 -0800

    Initial Setup

---
+++ Finger-Paint-App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Finger-Paint-App
+//
+//  Created by Dave Augerinos on 2017-02-18.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Finger-Paint-AppTests/Finger_Paint_AppTests.m
@@ -0,0 +1,39 @@
+//
+//  Finger_Paint_AppTests.m
+//  Finger-Paint-AppTests
+//
+//  Created by Dave Augerinos on 2017-02-18.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Finger_Paint_AppTests : XCTestCase
+
+@end
+
+@implementation Finger_Paint_AppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
